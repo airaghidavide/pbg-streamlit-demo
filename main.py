de657aee614afa84a3fcd21c5fb82a6e447dd80b
Davide Airaghi <airaghi.davide@gmail.com>
2022-04-07 22:52:17 +0200

    modifiche demo

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from app.src.common import load_data
 from app.src.common import make_plot
 
 st.set_page_config(
-     page_title="Chocolate Bar Rating",
-     layout="wide",
- )
-st.title('Chocolate Bar Rating')
-st.write('Chocolate is one of the most popular candies in the world. Each year, residents of the United States collectively eat more than 2.8 billions pounds. However, not all chocolate bars are created equal!')
-st.image('./static/choco_bar.jpg',width=300)
+    page_title = 'Chocolate bar rating',
+    layout = 'wide'
+)
 
-#load data for the app (all in cache)
+st.title('Chocolate bar rating')
+st.write('App demo PBG')
+st.image('static/choco_bar.jpg',width=300)
+
 df_choco = load_data.load_dataset()
 
 anno = st.sidebar.selectbox('Anno',sorted(df_choco.review_date.unique()))
 
-#create plot
-st.altair_chart(make_plot.make_heatmap(df_choco,anno), use_container_width=True)
+st.altair_chart(make_plot.make_heatmap(df_choco,anno),use_container_width=True)
+
